2a7d52cca70995e21223f042c429012b9180482e
alissa <as@iole.co.uk>
2017-03-07 14:25:39 -0800

    Code added

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,86 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if(answer.value === '' || attempt.value === '') {
+    	setHiddenFields();
+    }
+    if(validateInput(input.value)) {
+    	attempt.value++;
+    } else {
+    	return false;
+    }
+
+    if(getResults(input.value)) {
+    	setMessage("You Win! :)");
+    	showAnswer(true);
+    	showReplay();
+    } else if (attempt.value >= 10) {
+    	setMessage("You Lose! :(");
+    	showAnswer(false);
+    	showReplay();
+    } else {
+    	setMessage("Incorrect, try again.");
+    }
 }
 
 //implement new functions here
+function setHiddenFields () {
+	answer.value = Math.floor(Math.random() * 1000);
+	answer.value = answer.value.toString();
+	while(answer.value.length < 4) {
+		answer.value = '0' + answer.value;
+	}
+	attempt.value = 0;
+}
+
+function setMessage(msg) {
+	document.getElementById('message').innerHTML = msg;
+}
+
+function validateInput(res) {
+	if(res.length === 4) {
+		return true;
+	} else {
+		setMessage("Guesses must be exactly 4 characters long.");
+		return false;
+	}
+}
+
+function getResults(input) {
+	correct = 0;
+	resultDiv = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+	for (var i = 0; i < input.length; i++) {
+		if (input[i] == answer[i]) {
+			correct++;
+			resultDiv += '<span class="glyphicon glyphicon-ok"></span>';
+		} else if (answer.value.indexOf(input[i]) >= 0) {
+			resultDiv += '<span class="glyphicon glyphicon-transfer"></span>';
+		} else {
+			resultDiv += '<span class="glyphicon glyphicon-remove"></span>';
+		}
+	}
+	resultDiv += '</div></div>';
+	document.getElementById('results').innerHTML += resultDiv;
+
+	if(correct == 4) {
+		return true;
+	} else {
+		return false;
+	}
+
+}
+
+function showAnswer(won) {
+	let code = document.getElementById("code");
+	code.innerHTML = answer.value;
+	if(won) {
+		code.className = code.className + " success";
+	} else {
+		code.className = code.className + " failure";
+	}
+}
+
+function showReplay() {
+	document.getElementById("guessing-div").style.display = "none";
+	document.getElementById("replay-div").style.display = "block";
+}
